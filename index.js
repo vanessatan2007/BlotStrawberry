c05453fc887f6b8589bccd79adad319f5eda297a
Vanessa Tan <140679670+vanessatan2007@users.noreply.github.com>
2024-08-30 08:41:31 -0400

    random leaves

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*
+@title: BlotStrawberry
+@author: Vanessa Tan
+@snapshot: 1.png
+*/
 const width = 125
 const height = 125
 
@@ -6,6 +11,6 @@ setDocDimensions(width, height)
 const sharpness = bt.randIntInRange(35,42)
 const tall = bt.randIntInRange(85,92)
 const berry = bt.catmullRom([[75, 25], [90, sharpness], [100, 80], [75, tall], [50,80], [60,sharpness], [75, 25]])
-const leaves = bt.catmullRom([[75, tall], [50, tall+8], [47, tall+12], [75, tall], [62, tall+10],[65, tall+13], [75,tall], [72,tall + 9],[75, tall+14], [75, tall], [76, tall+5], [85, tall+12], [75, tall], [85, tall+7], [97, tall+11], [75, tall]])
+const leaves = bt.catmullRom([[75, tall], [bt.randIntInRange(47,52), tall+bt.randIntInRange(3,8)], [bt.randIntInRange(47,52), tall+bt.randIntInRange(9, 16)], [75, tall], [bt.randIntInRange(53,65), tall+bt.randIntInRange(8,12)],[bt.randIntInRange(53,65), tall+bt.randIntInRange(12,17)], [75,tall], [bt.randIntInRange(66,72),tall + bt.randIntInRange(6,11)],[bt.randIntInRange(68,78), tall+bt.randIntInRange(10,19)], [75, tall], [bt.randIntInRange(76,82), tall+bt.randIntInRange(3,9)], [bt.randIntInRange(80,87), tall+bt.randIntInRange(11,15)], [75, tall], [bt.randIntInRange(85,92), tall+bt.randIntInRange(5,12)], [bt.randIntInRange(94,100), tall+bt.randIntInRange(11,19)], [75, tall]])
 drawLines([berry])
 drawLines([leaves])
